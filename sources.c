cb9193216dad5b87c3dc97321e766b6eb436806b
owusu <112888945+GGerald269@users.noreply.github.com>
2023-03-10 12:38:12 -0800

    Loop addition

--- sources.c
+++ sources.c
@@ -3,4 +3,14 @@ int main(){
 
     printf("Hello, world!");
     printf("Who are you?");
+
+    int numbers;//initialization variables
+    printf("Please enter any number");
+    scanf("%d",numbers);
+
+    //loop for number incrementation
+    for (int i=0; i <= numbers; i++)
+    {
+        printf("%d",i);
+    }
 }
