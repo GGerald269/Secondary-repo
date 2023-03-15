d22c7cfa385503a43419203010a08c43c1cebbb7
owusu <112888945+GGerald269@users.noreply.github.com>
2023-03-15 07:58:05 +0000

    New revised code

--- sources.c
+++ sources.c
@@ -1,75 +1,74 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
+#include <math.h>
+#include <time.h>
 
-int main(){
+int main()
+{
 
+    // seed to generate random numbers is based of time
+    srand(time(0));
 
-
-    // printf("Hello, world!");
-    // printf("Who are you?");
-
-    // int numbers;//initialization variables
-    // printf("Please enter any number");
-    // scanf("%d",numbers);
-    
-    
-    // //loop for number incrementation
-    // for (int i=0; i <= numbers; i++)
-    // {
-    //     printf("%d",i);
-    // }
-
-
-    /*Division algorithm
-    For a positive integer "a" and "d" which is any integer 
-    there is a reminder r such that "0 < r < d" 
-      a = dq + r 
-    There anove is a verbal description of the algorithm*/
-
+    // declaration variables
     int userInput;
     int quotient;
-    int primeNumber = 50;
-    do //do while loop to insure correct input is entered
+
+    do // do while loop to insure correct input is entered
     {
         printf("\nEnter any positive integer:");
-        scanf("%d",&userInput);
+        scanf("%d", &userInput);
 
         if (userInput <= 0)
         {
             printf("Invalid input");
             printf("Please enter a positive integer\n");
         }
-        
-    } while ( userInput <= 0 || userInput >= 50);
 
-    //Boolean to check if the input is prime
+    } while (userInput <= 0 || userInput >= 50);
+
+    // Boolean to check if the input is prime
     int isPrime = true;
-    
-    //For a number that is prime it has multiples of 1 and itself 
-    //And also 
-    for (int u = 2 ; u <= primeNumber ; u++)
+
+    const int MIN = 2;
+    const int MAX = userInput / 2;
+    int primeNumber = (rand() % MAX) + MIN; // Generation of random numbers from 0 to the user's input
+
+    while (primeNumber % 2 == 0 || primeNumber == 2)
     {
-        for (int i = u; i > 1; i--)
+        printf("Checking for possible new random\n");
+        primeNumber++;
+    }
+
+    // condition to check if random number is prime
+    if (userInput / primeNumber != 0)
+    {
+        for (int i = primeNumber - 1; i > 1; i--)
         {
-            if (u % i == 0)
-            {
+            printf("%d\n", i);
+            if (primeNumber % i == 0)
                 isPrime = false;
-                break;
-            }
         }
     }
-   
-
-   //Condition to assign the quotient to the prime number
-    if (isPrime)
-        quotient = primeNumber;
     else
-        printf("It is not a prime number no possible output\n");
+    {
+        printf("Sorry code crashed");
+    }
 
+    // For a number that is prime it has multiples of 1 and itself
+    // And also
+    // int remainder = userInput - (primeNumber * );
+    if (isPrime)
+    {
+        int remainder = userInput % primeNumber;
+        int divisor = (userInput - remainder) / primeNumber;
+        printf("\nIt is prime\n");
+        printf("%d = (%d x %d ) + %d\n", userInput, divisor, primeNumber, remainder);
+    }
 
-    //initialzing the remainder and divisor to display to the screen
-    int reminder = userInput % quotient;
-    int divisor = (userInput - reminder)/quotient;
-
-    printf("%d = ( %d x %d ) + %d\n",userInput,divisor,quotient,reminder);
+    else
+    {
+        printf("The program selection is not a prime\n");
+        printf("Re-run the program\n");
+    }
 }
